73f86bec6de67952beb947f388d0cff587c24178
sanjay <sanjayy7890@gmail.com>
2022-12-20 16:34:23 -0500

    advanced js exercise1

--- 1_scope/scope..js
+++ 1_scope/scope..js
@@ -4,19 +4,19 @@ var fun = 5;
 
 function funFunction(){
     //child scope
-    var fun = "Hello";
+    var fun = "Bye";
     console.log(1, fun);
 }
 
 function funerFunction(){
     //child scope
-    var fun = "Bye";
+    var fun = "Hi";
     console.log(2, fun);
 }
 
 function funestFunction(){
     //child scope
-    fun = "Ahhhhhhh";
+    fun = "Hehe";
     console.log(3, fun);
 }
 
