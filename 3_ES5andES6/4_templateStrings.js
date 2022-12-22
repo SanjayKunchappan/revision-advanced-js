2d32c22b569ebeaf2c88178e5f4d369b9855c395
sanjay <sanjayy7890@gmail.com>
2022-12-21 22:14:44 -0500

    template strings

---
+++ 3_ES5andES6/4_templateStrings.js
@@ -0,0 +1,7 @@
+//template strings
+let name = "Khushi";
+let age = 28;
+let pet = "monkey";
+const greeting = "Hello " + name + ", what a great " + pet + " you have!";
+
+const greetingBest = `Hello ${name}, you seem to be ${age-10}! What a beautiful ${pet} you have!`;
